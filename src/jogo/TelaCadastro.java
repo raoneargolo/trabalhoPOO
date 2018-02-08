207b2c465a32a318c8ded5354e779aa163ee5491
Raone Argolo <raoneargolo@gmail.com>
2018-02-08 16:17:53 -0300

    Novos panels

--- src/jogo/TelaCadastro.java
+++ src/jogo/TelaCadastro.java
@@ -1,114 +1,116 @@
 package jogo;
-
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import java.awt.Font;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class TelaCadastro extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField textNome;
-	private JTextField textUsuario;
-	private JPasswordField senha;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaCadastro frame = new TelaCadastro();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+public class TelaCadastro extends JPanel {
+	private JPasswordField passwordField;
+	private JPasswordField passwordField_1;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
-	 * Create the frame.
+	 * Create the panel.
 	 */
-	public TelaCadastro() {
-		setResizable(false);
-		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
-		setBounds(100, 100, 661, 455);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//CAMPO PARA CHAMADA PARA A JANELA TELA INICIAL
-				TelaInicial obj = new TelaInicial();
-				obj.setVisible(true);
+	public TelaCadastro(final JFrame frame) { //em todo cabeçalho de panel, chama o frame
+
+		JLabel lblNewLabel = new JLabel("Nome");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+
+		JLabel lblNewLabel_1 = new JLabel("Usu\u00E1rio");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+
+		JLabel lblNewLabel_2 = new JLabel("Senha");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+
+		JLabel lblNewLabel_3 = new JLabel("Confirmar senha");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false); //setar o panel como falso
+				TelaInicial ti = new TelaInicial(frame); //crio objeto do proximo panel
+				frame.setContentPane(ti); //colocar no frame o proximo panel
 			}
 		});
-		
-		textNome = new JTextField();
-		textNome.setColumns(10);
-		
-		JLabel lblNome = new JLabel("Nome");
-		
-		JLabel lblNomeDeUsuario = new JLabel("Nome de Usuario");
-		
-		textUsuario = new JTextField();
-		textUsuario.setColumns(10);
-		
-		JLabel lblSenha = new JLabel("Senha");
-		
-		senha = new JPasswordField();
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(128)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(54)
-							.addComponent(btnSalvar))
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-							.addComponent(lblNome)
-							.addComponent(textNome, GroupLayout.DEFAULT_SIZE, 221, Short.MAX_VALUE)
-							.addComponent(lblNomeDeUsuario)
-							.addComponent(lblSenha)
-							.addComponent(textUsuario))
-						.addComponent(senha, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(184, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addGap(29)
-					.addComponent(lblNome)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textNome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(lblNomeDeUsuario)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(lblSenha)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(senha, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 88, Short.MAX_VALUE)
-					.addComponent(btnSalvar)
-					.addGap(37))
-		);
-		contentPane.setLayout(gl_contentPane);
+
+		passwordField = new JPasswordField();
+
+		passwordField_1 = new JPasswordField();
+
+		textField = new JTextField();
+		textField.setColumns(10);
+
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+
+		JLabel lblCadastro = new JLabel("Cadastro");
+		lblCadastro.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+										.addComponent(lblNewLabel_3).addPreferredGap(ComponentPlacement.UNRELATED)
+										.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 149,
+												GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.RELATED))
+								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addGroup(groupLayout.createSequentialGroup().addGap(112)
+														.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+																.addComponent(lblNewLabel).addComponent(lblNewLabel_1))
+														.addPreferredGap(ComponentPlacement.UNRELATED)
+														.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+																.addComponent(textField, 149, 149, Short.MAX_VALUE)
+																.addComponent(textField_1, GroupLayout.DEFAULT_SIZE,
+																		149, Short.MAX_VALUE)))
+												.addGroup(groupLayout.createSequentialGroup().addGap(199)
+														.addComponent(lblCadastro)))
+										.addGroup(groupLayout.createSequentialGroup().addGap(123)
+												.addComponent(lblNewLabel_2)
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addComponent(passwordField_1, 149, 149, 149))))
+						.addGap(175))
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup().addGap(210).addComponent(btnNewButton)
+						.addContainerGap(209, Short.MAX_VALUE)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
+				.createSequentialGroup().addGap(22)
+				.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup().addComponent(lblCadastro).addGap(84).addComponent(
+								textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE))
+						.addComponent(lblNewLabel))
+				.addGap(30)
+				.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1))
+				.addGap(30)
+				.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(passwordField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_2))
+				.addGap(28)
+				.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_3))
+				.addPreferredGap(ComponentPlacement.RELATED, 96, Short.MAX_VALUE).addComponent(btnNewButton)
+				.addGap(75)));
+		setLayout(groupLayout);
+
 	}
+
 }
--- src/jogo/TelaInicial.java
+++ src/jogo/TelaInicial.java
@@ -1,168 +1,61 @@
 package jogo;
-
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.TableColumn;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JTextField;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.Font;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class TelaInicial extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField textFieldDerrotas;
-	private JTextField textFieldEmpates;
-	private JTextField textFieldVitorias;
-	private JTable table;
+public class TelaInicial extends JPanel {
 
 	/**
-	 * Launch the application.
+	 * Create the panel.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaInicial frame = new TelaInicial();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+	public TelaInicial(JFrame frame) {
+		
+		JLabel lblNewLabel = new JLabel("XXXXXX");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		
+		JLabel lblNewLabel_1 = new JLabel(", bem vindo");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				TelaCadastro tC = new TelaCadastro(frame);
+				frame.setContentPane(tC);
 			}
 		});
-	}
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnVoltar)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblNewLabel_1)))
+					.addContainerGap(233, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(45)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(lblNewLabel_1))
+					.addGap(58)
+					.addComponent(btnVoltar)
+					.addContainerGap(325, Short.MAX_VALUE))
+		);
+		setLayout(groupLayout);
 
-	/**
-	 * Create the frame.
-	 */
-	public TelaInicial() {
-		setResizable(false);
-		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 684, 481);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnInicio = new JMenu("Inicio");
-		menuBar.add(mnInicio);
-		
-		JMenuItem mntmNovoJogo = new JMenuItem("Novo Jogo");
-		mnInicio.add(mntmNovoJogo);
-		
-		mntmNovoJogo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				teste T1 = new teste(); // chamando a jenela do jogo
-				T1.setVisible(true);
-			}
-		});
-		
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Sair");
-		mntmNewMenuItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// TODO adicionar açao de fechar a app
-				
-				System.exit(0); // Comando para fechar a janela atual
-	
-			}
-		});
-		mnInicio.add(mntmNewMenuItem);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		textFieldDerrotas = new JTextField();
-		textFieldDerrotas.setColumns(10);
-		
-		textFieldEmpates = new JTextField();
-		textFieldEmpates.setColumns(10);
-		
-		textFieldVitorias = new JTextField();
-		textFieldVitorias.setColumns(10);
-		
-		JLabel lblDerrotas = new JLabel("Derrotas");
-		
-		JLabel lblEmpates = new JLabel("Empates");
-		
-		JLabel lblVitorias = new JLabel("Vitorias");
-		
-		table = new JTable(11,3);
-		
-		String[] columnNames = {"Vitoria","Derrota","Empate"};
-		Object[][] data = {
-			    {"Vitoria","Derrota","Empate"},
-			    {"John", "Doe","Rowing", new Integer(3), new Boolean(true)},
-			    {"Sue", "Black","Knitting", new Integer(2), new Boolean(false)},
-			    {"Jane", "White","Speed reading", new Integer(20), new Boolean(true)},
-			    {"Joe", "Brown","Pool", new Integer(10), new Boolean(false)}
-			};
-		
-		JTable table = new JTable(data, columnNames);
-		
-		JLabel lblUltimosJogos = new JLabel("Ultimos 10 Jogos");
-		
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(54)
-							.addComponent(table, GroupLayout.PREFERRED_SIZE, 326, GroupLayout.PREFERRED_SIZE)
-							.addGap(79)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(textFieldVitorias, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblVitorias)
-									.addGap(32))
-								.addComponent(textFieldDerrotas, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(textFieldEmpates, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblDerrotas)
-									.addGap(31))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblEmpates)
-									.addGap(28))))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(168)
-							.addComponent(lblUltimosJogos)))
-					.addContainerGap(40, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(26)
-					.addComponent(lblUltimosJogos)
-					.addGap(4)
-					.addComponent(table, GroupLayout.DEFAULT_SIZE, 327, Short.MAX_VALUE)
-					.addGap(23))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(114, Short.MAX_VALUE)
-					.addComponent(lblVitorias)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textFieldVitorias, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(29)
-					.addComponent(lblEmpates)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textFieldEmpates, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(22)
-					.addComponent(lblDerrotas)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textFieldDerrotas, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(110))
-		);
-		contentPane.setLayout(gl_contentPane);
 	}
 }
