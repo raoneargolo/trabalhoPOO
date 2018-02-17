0c3465c46716c98d526bf4c4f8db6eead77a9dc1
eric <erick.gtn.xp@gmail.com>
2018-02-17 12:31:42 -0300

    Responsável pelo main e conectar ao servidor

---
+++ src/jogo/Cliente.java
@@ -0,0 +1,40 @@
+package jogo;
+
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+public class Cliente /* implements Runnable */ {
+
+	public static void main(String args[]) throws UnknownHostException, IOException, ClassNotFoundException {
+		TelaLogon frame;
+		Socket cliente = new Socket("127.0.0.1", 12346);
+
+		//LerUsuario objLerUsuario = new LerUsuario();
+		Map<String, String> mapaUsuarios; /*= objLerUsuario.lerUsuarios();*/
+		
+		ObjectOutputStream outToServer = new ObjectOutputStream(cliente.getOutputStream());
+		
+		ObjectInputStream inFromServer = new ObjectInputStream(cliente.getInputStream());
+		mapaUsuarios=(Map<String, String>)inFromServer.readObject();
+		frame = new TelaLogon(mapaUsuarios, outToServer);
+		frame.setVisible(true);
+		cliente.close();
+	}
+}
+
+/*public static void main(String[] args) {
+LerUsuario objLerUsuario = new LerUsuario();
+mapaUsuarios = objLerUsuario.lerUsuarios();
+
+EventQueue.invokeLater(new Runnable() {
+	public void run() {
+		frame = new TelaLogon();
+		frame.setVisible(true);
+	}
+});
+}*/
