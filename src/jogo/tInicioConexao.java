b36614b4eaa779df5d56631ee90368ea41ae7c99
eric <erick.gtn.xp@gmail.com>
2018-02-17 12:33:40 -0300

    Thread de início funcional

---
+++ src/jogo/tInicioConexao.java
@@ -0,0 +1,49 @@
+package jogo;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Map;
+import java.util.Scanner;
+
+public class tInicioConexao implements Runnable {
+	Socket cliente;
+	Map<String, String> mapaU;
+	String inputFromClient;
+	String buffer[];
+	
+	public tInicioConexao(Socket cliente, Map<String, String> mapaU) throws IOException {
+		this.cliente=cliente;
+		this.mapaU=mapaU;		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			ObjectOutputStream outToClient = new ObjectOutputStream(cliente.getOutputStream());
+			outToClient.flush();
+			//ObjectInputStream inFromClient = new ObjectInputStream(cliente.getInputStream());
+			outToClient.writeObject(mapaU);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/*try {
+			//ObjectOutputStream outToClient = new ObjectOutputStream(cliente.getOutputStream());
+			ObjectInputStream inFromClient = new ObjectInputStream(cliente.getInputStream());
+				inputFromClient=(String)inFromClient.readObject();
+				buffer=inputFromClient.split(":");
+				new TratamentoDeUsuarios().cadastroEmArquivo(buffer[0], buffer[1]);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+	}
+}
